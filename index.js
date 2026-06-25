68b0a7ba94df6449c916e2828632ef04058b3af3
Prachi-3101 <prachistudies22@gmail.com>
2026-06-25 11:06:45 +0530

    Refactor CORS configuration to simplify origin handling

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const app = express();
 
 await connectDB();
 
-app.use(cors({
- origin: [
-  process.env.CLIENT_URL || 'http://localhost:5173',
-  "https://edutrack-azure.vercel.app/"
-],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
